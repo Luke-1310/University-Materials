d5faa58e33a6ecd0602036ee65eef8a8e04874aa
Luke-1310 <priviteraluca2001@gmail.com>
2023-11-12 11:04:30 +0100

    .

--- Tesina/res/PHP/aggiungi_recensione.php
+++ Tesina/res/PHP/aggiungi_recensione.php
@@ -1,108 +1,79 @@
 <?php
-    session_start();
 
-    include("connection.php");
+session_start();
 
-    $connessione = new mysqli($host, $user, $password, $db);
+include("connection.php");
+include('funzioni.php');
 
-    $recensione_testo = $_POST['recensione'];
+$connessione = new mysqli($host, $user, $password, $db);
 
-    //presa la recensione tramite $_POST procediamo con l'inserimento(una volta che avremmo recuperato tutti i dati)
-    //i dati da recuperare sono l'ID dell'utente registrato,
-    $xmlfile = "../XML/catalogo.xml";  //Percorso del file XML
+$recensoreID = $_POST['ID'];
+$recensione_testo = $_POST['recensione'];
     
-    //tramite funzioni.php mi carico i fumetti, cerco quello corretto e inserisco la recensione
-    include('funzioni.php');
+$query = "SELECT umn.reputazione FROM utenteDati ud  INNER JOIN utenteMangaNett umn ON ud.id = umn.id  WHERE umn.username = '{$_SESSION["nome"]}'";
+$result = $connessione->query($query);
 
-    $fumetti = getFumetti($xmlfile);
+if ($result) {
 
-    //mi prendo innanzitutto l'ID dell'utente loggato
-    $query = "SELECT umn.id FROM utenteDati ud  INNER JOIN utenteMangaNett umn ON ud.id = umn.id  WHERE umn.username = '{$_SESSION["nome"]}'";
+    $row = $result->fetch_assoc();
+    $reputazione_recensore = $row['reputazione'];
+} 
+else {
+    echo "Errore nella query: " . $connessione->error;
+}
 
-    $result = $connessione->query($query);
+$xmlpath = "../XML/catalogo.xml";
+$xmlstring = "";
 
-    //Verifico se la query ha restituito risultati
-    if ($result) {
+foreach(file($xmlpath) as $nodo){   //Leggo il contenuto del file XML
 
-        //Estraggo il risultato come un array associativo
-        $row = $result->fetch_assoc();
-        $id = $row['id'];
-    } 
-    
-    else {
-        echo "Errore nella query: " . $connessione->error;
-    }
+    $xmlstring.= trim($nodo); 
+}
+
+$document = new DOMDocument();
+$document->loadXML($xmlstring);
+
+$fumetti_doc = $document->getElementsByTagName('fumetto');
+
+foreach ($fumetti_doc as $fumetto_doc) {
+
+    $fumetto_titolo = $fumetto_doc->getElementsByTagName('titolo')->item(0)->nodeValue;
+
+    if ($_SESSION['info_titolo'] === $fumetto_titolo) {
+
+        $recensioneElement = $document->createElement('recensione');
+
+        $recensoreIDElement = $document->createElement('IDRecensore', $recensoreID);
+        $recensioneElement->appendChild($recensoreIDElement);
+
+        $dataRec = date('Y-m-d\TH:i:s');
+        $dataElement = $document->createElement('dataRecensione', $dataRec);
+        $recensioneElement->appendChild($dataElement);
+
+        $testoElement = $document->createElement('testoRecensione', $recensione_testo);
+        $recensioneElement->appendChild($testoElement);
+
+        $reputazioneElement = $document->createElement('reputazioneRecensore', $reputazione_recensore);
+        $recensioneElement->appendChild($reputazioneElement);
         
-    //mi prendo la reputazione dell'utente
-    $query_repu = "SELECT umn.reputazione FROM utenteDati ud  INNER JOIN utenteMangaNett umn ON ud.id = umn.id  WHERE umn.username = '{$_SESSION["nome"]}'";
+        $segnalazione = "-1";
+        $segnalazioneElement = $document->createElement('segnalazione', $segnalazione);
+        $recensioneElement->appendChild($segnalazioneElement);
 
-    $result_repu = $connessione->query($query);
+        $IDSegnalatore = "-1";
+        $IDSegnalatoreElement = $document->createElement('IDSegnalatore', $IDSegnalatore);
+        $recensioneElement->appendChild($IDSegnalatoreElement);
 
-    //Verifico se la query ha restituito risultati
-    if ($result_repu) {
-
-        //Estraggo il risultato come un array associativo
-        $row = $result_repu->fetch_assoc();
-        $repu = $row['reputazione'];
-    } 
-    
-    else {
-        echo "Errore nella query: " . $connessione->error;
+        $fumetto_doc->appendChild($recensioneElement);
+        break;
     }
+}
 
-    //mi prendo la data e l'ora, ora ho tutto, ID, recensione, data e ora e reputazione
-    $dataRec = date('Y-m-d\TH:i:s');
+$document->formatOutput = true; 
+$xml = $document->saveXML();
 
-    $recensione = [
-        'utenteID' => $id,
-        'rec' => $recensione_testo,
-        'dataRec' => $dataRec,
-        'reputazionevotante' => $repu,
-    ];
-        
-    //bene, ora che l'array che contiene i fumetti è stato aggiornato bisogna aggiornare il file XML 
-    $document = new DOMDocument();
-    $document->load($xmlfile);
+file_put_contents($xmlpath, $xml); 
 
-    //Mi prendo tutti gli elementi 'fumetto' nel documento
-    $fumetti_doc = $document->getElementsByTagName('fumetto');
-
-    foreach ($fumetti_doc as $fumetto_doc) {
-        //Ottieni l'attributo 'titolo' dell'elemento 'fumetto'
-        $fumetto_titolo = $fumetto_doc->getElementsByTagName('titolo')->item(0)->nodeValue;
-
-        //Ora devo inserire la recensione nel fumetto corretto
-        if ($_SESSION['info_titolo'] === $fumetto_titolo) {
-
-            // Crea un nuovo elemento recensione
-            $recensioneElement = $document->createElement('recensione');
-            
-            // Aggiungi l'ID dell'utente
-            $utenteIDElement = $document->createElement('utenteID', $id);
-            $recensioneElement->appendChild($utenteIDElement);
-
-            // Aggiungi il testo della recensione
-            $recElement = $document->createElement('rec', $recensione_testo);
-            $recensioneElement->appendChild($recElement);
-
-            // Aggiungi la data della recensione
-            $dataRecElement = $document->createElement('dataRec', $dataRec);
-            $recensioneElement->appendChild($dataRecElement);
-
-            // Aggiungi la reputazione del votante
-            $reputazioneVotanteElement = $document->createElement('reputazionevotante', $repu);
-            $recensioneElement->appendChild($reputazioneVotanteElement);
-
-            // Aggiungi l'elemento recensione al fumetto
-            $fumetto_doc->appendChild($recensioneElement);
-
-            break; 
-        }
-    }
-
-    // Salva il documento XML aggiornato nel file
-    $document->save($xmlfile);
-
-
+$_SESSION['richiesta_ok']= true;
 header('Location:../../prodotti_info.php');
 ?>
