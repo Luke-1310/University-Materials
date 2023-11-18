65a956cd319785f9c57c7b8ba076d4a5db353d9e
Luke-1310 <priviteraluca2001@gmail.com>
2023-11-18 23:29:35 +0100

    .

---
+++ Tesina/res/PHP/modifica_offerta.php
@@ -0,0 +1,20 @@
+<?php
+
+session_start();
+
+$titolo = $_POST['titolo'];
+
+$sc_X = $_POST['registrazione_mesi'];
+$sc_Y = $_POST['registrazione_anni'];
+$sc_M = $_POST['crediti_data'];
+$sc_data_M = $_POST['da_data'];
+$sc_N = $_POST['crediti'];
+$sc_R = $_POST['reputazione'];
+$sc_ha_acquistato = $_POST['ha_acquistato'];
+
+$sc_bonus = $_POST['bonus'];
+$sc_generico = $_POST['generico'];
+
+//DA FINIRE
+
+?>
