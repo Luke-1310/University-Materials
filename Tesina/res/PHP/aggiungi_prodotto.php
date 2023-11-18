fc532ad61237fb86d8c612cd33837d2ffa5b7b63
Luke-1310 <priviteraluca2001@gmail.com>
2023-11-18 23:29:15 +0100

    .

--- Tesina/res/PHP/aggiungi_prodotto.php
+++ Tesina/res/PHP/aggiungi_prodotto.php
@@ -14,16 +14,6 @@ $date = $_POST['data'];
 $quantity = $_POST['quantita'];
 $publisher = $_POST['editrice'];
 
-// $sc_X = $_POST['registrazione_mesi'];
-// $sc_Y = $_POST['registrazione_anni'];
-// $sc_M = $_POST['crediti_data'];
-// $sc_data_M = $_POST['da_data'];
-// $sc_N = $_POST['crediti'];
-// $sc_R = $_POST['reputazione'];
-
-// $sc_bonus = $_POST['bonus'];
-// $sc_generico = $_POST['generico'];
-
 //come prima cosa mi voglio ricavare il valore che andrà nel campo img del file XML
 //controllo se l'estensione del file è di tipo .jpg
 $dirDestinazione = "../WEBSITE_MEDIA/PRODUCT_MEDIA/"; //Assegna la cartella di destinazione delle immagini
