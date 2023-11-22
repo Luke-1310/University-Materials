5ed4e8d04e8ae237fa40258889fb7b6c0bb36a1b
Luke-1310 <priviteraluca2001@gmail.com>
2023-11-22 23:47:31 +0100

    first implemention of carrello.php

---
+++ Tesina/carrello.php
@@ -0,0 +1,119 @@
+<?php
+   session_start();
+
+   if(isset($_COOKIE["tema"]) && $_COOKIE["tema"] == "scuro"){
+       echo "<link rel=\"stylesheet\" href=\"res/CSS/external_carrello_dark.css\" type=\"text/css\" />";
+   }
+   else{
+       echo "<link rel=\"stylesheet\" href=\"res/CSS/external_carrello.css\" type=\"text/css\" />";
+   }
+?>
+<?php 
+    echo "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>"; 
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+<!-- titolo della scheda -->
+<head>
+    <title>Carrello</title>
+</head>
+    
+<?php 
+    $pagina_corrente = "carrello";
+    include('res/PHP/navbar.php');
+    include('res/PHP/connection.php');
+    include('res/PHP/funzioni.php');
+?>
+
+<body>
+
+    <?php
+
+        $xmlPath = "res/XML/catalogo.xml";
+        $fumetti = getFumetti($xmlPath);
+
+        $pathImg = "res/WEBSITE_MEDIA/PRODUCT_MEDIA/";
+        $ext = ".jpg";
+
+        // unset($_SESSION['carrello']);
+        if(isset($_SESSION['carrello']) && !empty($_SESSION['carrello'])){
+
+            echo "<div class=\"container\">";
+
+                echo "<div class=\"column\">";
+                    echo "<h4>PRODOTTO</h4>";
+                echo"</div>";
+
+                echo "<div class=\"column\">";
+                    echo "<h4>TITOLO</h4>";
+                echo"</div>";
+
+                echo "<div class=\"column\">";
+                    echo "<h4>QUANTITÀ</h4>";
+                echo"</div>";
+
+                echo "<div class=\"column\">";
+                    echo "<h4>BONUS</h4>";
+                echo"</div>";
+
+                echo "<div class=\"column\">";
+                    echo "<h4>PREZZO</h4>";
+                echo"</div>";
+
+                echo "<div class=\"column\">";
+                    echo "<h4>PREZZO SCONTATO</h4>";
+                echo"</div>";
+
+                foreach($_SESSION['carrello'] as $fumetto_carrello) {
+                    
+                    foreach($fumetti as $fumetto){
+                        
+                        if($fumetto['isbn'] == $fumetto_carrello['isbn']){
+
+                            $nomeImg = $fumetto_carrello['isbn'] . $ext;
+
+                            echo "<div class=\"column\">";
+                                echo "<img src='" . $pathImg . $nomeImg . "' alt=\"Copertina.jpg\">";
+                            echo"</div>";
+
+                            echo "<div class=\"column\">";
+                                echo $fumetto['titolo'];
+                            echo"</div>";
+
+                            echo "<div class=\"column\">";
+                                echo $fumetto_carrello['quantita'];
+                            echo"</div>";
+
+                            echo "<div class=\"column\">";
+                                echo $fumetto['bonus'] . " CR";
+                            echo"</div>";
+
+                            echo "<div class=\"column\">";
+                                echo $fumetto['prezzo'] ." CR";
+                            echo"</div>";
+
+                            echo "<div class=\"column\">";
+                                echo "PREZZO SCONTATO";
+                            echo"</div>";
+                        }
+                    }
+                }
+
+            echo "</div>";
+        }
+
+        else{
+            echo "<p id=\"no_response\">NESSUN PRODOTTO AGGIUNTO... ¯\_(ツ)_/¯</p>";
+        }
+    ?>
+
+</body>
+
+<?php include('res/PHP/footer.php')?>
+
+
+</html>
--- Tesina/res/PHP/carrello.php
+++ Tesina/res/PHP/carrello.php
@@ -2,18 +2,40 @@
 
 session_start();
 
-$fumetto_isbn = $_POST['isbn'];
+$fumetto_isbn_POST = $_POST['isbn'];
+$isPresente = false;
 
-// $fumetto_prezzo = $_POST['prezzo'];
+//controllo se è già stato aggiunto, in modo tale da non aggiungere un nuovo elemento ma aggiorno solo la quantità
+if(!empty($_SESSION['carrello'])){
 
-// //creo un array con i dati del POST
-// $fumetto = array(
-//     'isbn' => $fumetto_isbn,
-//     'prezzo' => $fumetto_prezzo
-// );
+    echo 0;
 
-//il [] alla fine serve per aggiungere l'elemento alla coda
-$_SESSION['carrello'][] = $fumetto_isbn;
+    foreach($_SESSION['carrello'] as $fumetto_carrello){
 
-header('Location: ../../carrello.php');
+        echo $fumetto_carrello['isbn'];
+        echo $fumetto_isbn_POST;
+
+        if($fumetto_carrello['isbn'] == $fumetto_isbn_POST){
+            $fumetto_carrello['quantita'] += 1;
+            $isPresente = true;
+            break;
+        }
+    }
+}
+
+if(!$isPresente){
+
+    //creo un array con i dati del POST, faccio perché voglio salvare diverse informazioni del fumetto
+    $fumetto = array(
+        'isbn' => $fumetto_isbn_POST,
+        'quantita' => 1
+    );
+
+    //il [] alla fine serve per aggiungere l'elemento alla coda
+    $_SESSION['carrello'][] = $fumetto;
+}
+
+$_SESSION['prodotto_aggiunto'] = true;
+
+// header('Location: ../../catalogo.php');
 ?>
