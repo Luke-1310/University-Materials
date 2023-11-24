98036f8f34029538c99f82c30f634321d02302cb
Luke-1310 <priviteraluca2001@gmail.com>
2023-11-24 11:14:33 +0100

    cacolaSpesaNoSconto implemented

--- Tesina/res/PHP/funzioni.php
+++ Tesina/res/PHP/funzioni.php
@@ -938,4 +938,28 @@
             echo "</div>";
         }
     }
+
+    //funzione per sommare la quantita di crediti per completare l'acquisto
+    function calcolaSpesaNoSconto(){
+
+        if (isset($_SESSION['carrello'])) {
+
+            $somma_totale = 0;
+            
+            foreach ($_SESSION['carrello'] as $fumetto_carrello) {
+        
+                $quantita = $fumetto_carrello['quantita'];
+                $prezzo = $fumetto_carrello['prezzo'];
+
+                $somma_totale += $prezzo * $quantita;
+
+            }
+        }
+
+        //in questo modo non mi approssima i numeri => ex 19.13 in 19.1
+        $somma_totale = number_format($somma_totale, 2, '.', '');
+        return $somma_totale;
+    }
+    
+
 ?>
