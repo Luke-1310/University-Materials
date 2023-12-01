2b9f73e506ebaf14e7bf8e8f85d48bbf3bdc4dd2
Luke-1310 <priviteraluca2001@gmail.com>
2023-12-01 22:33:16 +0100

    .

--- Tesina/res/PHP/elimina_prodotto.php
+++ Tesina/res/PHP/elimina_prodotto.php
@@ -13,28 +13,54 @@ $document->load($pathXml);
 //Mi prendo tutti gli elementi 'fumetto' nel documento
 $fumetti_doc = $document->getElementsByTagName('fumetto');
 
-    foreach ($fumetti_doc as $fumetto_doc) {
+foreach ($fumetti_doc as $fumetto_doc) {
 
-        $fumetto_isbn = $fumetto_doc->getAttribute('isbn');
+    $fumetto_isbn = $fumetto_doc->getAttribute('isbn');
 
-        if ($ISBN_prodotto === $fumetto_isbn){
+    if ($ISBN_prodotto === $fumetto_isbn){
 
-            $fumettoDaRimuovere = $fumetto_doc;
-            $fumettoDaRimuovere->parentNode->removeChild($fumettoDaRimuovere);
-            break;
+        // $fumettoDaRimuovere = $fumetto_doc;
+        // $fumettoDaRimuovere->parentNode->removeChild($fumettoDaRimuovere);
 
-            $immagineDaEliminare = "../WEBSITE_MEDIA/PRODUCT_MEDIA/{$ISBN_prodotto}.jpg";
+        $immagineDaEliminare = "../WEBSITE_MEDIA/PRODUCT_MEDIA/{$ISBN_prodotto}.jpg";
 
-            if (file_exists($immagineDaEliminare)) {
-                unlink($immagineDaEliminare);
-            }
-        }
+        // if (file_exists($immagineDaEliminare)) {
+        //     unlink($immagineDaEliminare);
+        // }
+
+        break;
     }
+}
 
 
 // Salva il documento XML aggiornato nel file
 $document->save($pathXml);
 
-// header('Location:../../prodotti_info.php');
+//ora cancello qualsiasi domanda con quel isbn, cancellando la domanda cancello anche le relative risposte e valutazioni
+$pathXml = "../XML/Q&A.xml";
+
+$document = new DOMDocument();
+$document->load($pathXml);
+
+$domande_doc = $document->getElementsByTagName('domanda');
+
+foreach ($domande_doc as $domanda_doc) {
+
+    $domanda_isbn = $domanda_doc->getElementsByTagName('ISBNProdotto');
+
+    if ($ISBN_prodotto === $domanda_isbn){
+
+        // $domandaDaRimuovere = $domanda_doc;
+        // $domandaDaRimuovere->parentNode->removeChild($domandaDaRimuovere);
+    }
+}
+
+
+// Salva il documento XML aggiornato nel file
+$document->save($pathXml);
+
+$_SESSION['prodotto_eliminato'] = true;
+
+header('Location:../../catalogo.php');
 
 ?>
