20da81825e56f28bdbc6a2371dc29a47af4411fd
Luke-1310 <priviteraluca2001@gmail.com>
2023-12-20 11:45:17 +0100

    .

--- Tesina/res/PHP/promuovi_retrocedi.php
+++ Tesina/res/PHP/promuovi_retrocedi.php
@@ -3,6 +3,7 @@
 session_start();
 
 include('connection.php');
+include('funzioni.php');
 
 $id = $_POST['id'];
 $ruolo = $_POST['ruolo'];
@@ -21,7 +22,7 @@ if (isset($_POST['bottone_promuovi'])) {
     $query_u = "UPDATE utenteMangaNett 
                 SET ruolo = '$ruolo'
                 WHERE id  = '$id' AND id <> 1";
-    //"id <> 1" DEVE essere diverso da 1, questo per tutelare il fatto che ci sia almeno un admin!
+    //"id <> 1" DEVE essere diverso da 1, questo per tutelare il superadmin!
 
     $result_u = $connessione->query($query_u);
 
@@ -31,6 +32,19 @@ if (isset($_POST['bottone_promuovi'])) {
         exit(1);
     }
 
+    //ora devo mettere la reputazione a 12, di base la promozione copre sia il ruolo di GS che AM e quindi sicuro tale ruoli deve avere reputazione a 12
+    $query_repu = "UPDATE utenteMangaNett 
+                SET reputazione = '12'
+                WHERE id  = '$id' AND id <> 1";
+
+    $result_repu = $connessione->query($query_repu);
+
+    //Verifico se la query ha restituito risultati
+    if (!$result_repu) {
+        echo "Errore nella query: " . $connessione->error;
+        exit(1);
+    }
+    
     header('Location:../../lista_utenti.php');
     exit(1);
 } 
@@ -41,13 +55,32 @@ if (isset($_POST['bottone_retrocedi'])) {
         $ruolo = "GS";
     }
     else if($ruolo == "GS"){
+
         $ruolo = "CL";
+
+        $xmlPath = "../XML/Q&A.xml";
+        $xmlPathFumetti = "../XML/catalogo.xml";
+
+        $reputazione = calcolaReputazione($id,$xmlPath,$xmlPathFumetti);
+
+        //in questo caso devo anche modificare la reputazione, un cliente non può avere reputazione 12
+        $query_repu = "UPDATE utenteMangaNett 
+        SET reputazione = '$reputazione'
+        WHERE id  = '$id' AND id <> 1";
+
+        $result_repu = $connessione->query($query_repu);
+
+        //Verifico se la query ha restituito risultati
+        if (!$result_repu) {
+            echo "Errore nella query: " . $connessione->error;
+            exit(1);
+        }
     }
 
     $query_u = "UPDATE utenteMangaNett 
                 SET ruolo = '$ruolo'
                 WHERE id  = '$id' AND id <> 1";
-    //"id <> 1" DEVE essere diverso da 1, questo per tutelare il fatto che ci sia almeno un admin!
+    //"id <> 1" DEVE essere diverso da 1, questo per tutelare il superadmin!
     
     $result_u = $connessione->query($query_u);
 
