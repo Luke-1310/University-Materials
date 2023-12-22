78108b24d112b7f37cfbbf97d3f99d07e1463634
Luke-1310 <priviteraluca2001@gmail.com>
2023-12-22 16:26:25 +0100

    .

---
+++ TEST/test DTD/homepage.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Form Libri</title>
+</head>
+<body>
+
+<form action="processa_dati.php" method="post">
+    <fieldset>
+        <legend>Libro</legend>
+        
+        <div>
+            <label for="isbn">ISBN:</label>
+            <input type="text" id="isbn" name="isbn" required>
+        </div>
+        
+        <div>
+            <label for="rating">Rating:</label>
+            <select id="rating" name="rating">
+                <option value="sufficiente">Sufficiente</option>
+                <option value="buono">Buono</option>
+                <option value="ottimo">Ottimo</option>
+            </select>
+        </div>
+        
+        <div>
+            <label for="nomeAutore">Nome Autore:</label>
+            <input type="text" id="nomeAutore" name="nomeAutore" required>
+        </div>
+        
+        <div>
+            <label for="cognomeAutore">Cognome Autore:</label>
+            <input type="text" id="cognomeAutore" name="cognomeAutore" required>
+        </div>
+        
+        <div>
+            <label for="titolo">Titolo:</label>
+            <input type="text" id="titolo" name="titolo" required>
+        </div>
+        
+        <div>
+            <label for="anno">Anno:</label>
+            <input type="number" id="anno" name="anno">
+        </div>
+        
+        <div>
+            <label for="editore">Editore:</label>
+            <input type="text" id="editore" name="editore" required>
+        </div>
+        
+        <div>
+            <input type="submit" value="Aggiungi Libro">
+        </div>
+    </fieldset>
+</form>
+
+</body>
+</html>
